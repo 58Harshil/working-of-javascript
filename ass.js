61a2e231670c46da4eb0e77621697bb976c50e77
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-06-08 17:24:40 +0530

    Create ass.js

---
+++ ass.js
@@ -0,0 +1,41 @@
+//1
+//console.log(addNumber(4,6));
+function addNumber(num1,num2){
+    return num1+num2
+}
+
+//2
+//console.log(mulNumber(4,6));
+const mulNumber = function(num1,num2){
+    return num1*num2
+}
+
+//3
+function addNumbers(num1,num2){
+    //console.log(sum);
+    var sum = num1 + num2
+    return sum 
+}
+//console.log(addNumbers(32,43));
+
+//4
+{
+   // console.log(x);
+   // console.log(y);
+   // console.log(z);
+
+    var x = "Harshil"
+    let y = "Choudhary"
+    const z = "Surat"
+    
+   // console.log(x);
+   // console.log(y);
+   // console.log(z);
+}
+
+//5
+{
+    //console.log(x);
+    let x = "Choudhary"
+    console.log(x);
+}
